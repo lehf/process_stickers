5546a47fd9f9c13eb5c688b780bd061e7a0ff333
lihf <1764129339@qq.com>
2023-03-13 17:22:25 +0800

    fix

--- setup.go
+++ setup.go
@@ -13,6 +13,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -28,13 +30,13 @@ func processImage(filePath string) {
 		log.Println(err, "filePath")
 	}
 	if fileInfo.IsDir() {
-		files, filesErr := ioutil.ReadDir(filePath)
-		if filesErr != nil {
-			log.Println(filePath)
-		}
-		for _, file := range files {
-			processImage(filePath + "/" + file.Name())
-		}
+		// files, filesErr := ioutil.ReadDir(filePath)
+		// if filesErr != nil {
+		// 	log.Println(filePath)
+		// }
+		// for _, file := range files {
+		// 	processImage(filePath + "/" + file.Name())
+		// }
 
 	} else {
 		pic, picerr := os.Open(filePath)
@@ -47,31 +49,52 @@ func processImage(filePath string) {
 				log.Println("Cannot decode file:", filePath)
 			} else {
 				if format == "png" || format == "jpg" || format == "jpeg" {
-					w, h := img.Bounds().Dx(), img.Bounds().Dy()
-					if w == 0 || h == 0 {
-						log.Println("Invalid image size:", w, h)
-					} else {
-						if w == h {
-							output, _ := os.Create(filePath + "_resized.png")
-							defer output.Close()
-							dst := image.NewRGBA(image.Rect(0, 0, 120, 120))
-							draw.CatmullRom.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
-							log.Println("Image resizing:", output)
-							png.Encode(output, dst)
-						}
-						log.Println(filePath+"Image size:", w, h)
-					}
+					log.Println("Image size:", img.Bounds().Dx())
+					// w, h := img.Bounds().Dx(), img.Bounds().Dy()
+					// if w == 0 || h == 0 {
+					// 	log.Println("Invalid image size:", w, h)
+					// } else {
+					// 	if w == h {
+					// 		output, _ := os.Create(filePath + "_resized.png")
+					// 		defer output.Close()
+					// 		dst := image.NewRGBA(image.Rect(0, 0, 120, 120))
+					// 		draw.CatmullRom.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
+					// 		log.Println("Image resizing:", output)
+					// 		png.Encode(output, dst)
+					// 	}
+					// 	log.Println(filePath+"Image size:", w, h)
+					// }
 				} else if format == "gif" {
 					pic.Seek(0, 0)
-					output, _ := os.Create(filePath + "_resized.gif")
+					otpth := strings.TrimSuffix(filePath, filepath.Ext(filePath))
+					thuotpth := strings.TrimSuffix(filePath, filepath.Ext(filePath))
+					output, _ := os.Create(otpth + "_resized.gif")
+					thumboutput, _ := os.Create(thuotpth + "_thumb.png")
 					defer output.Close()
+					defer thumboutput.Close()
+
 					tx := func(m image.Image) image.Image {
 						return resize.Resize(240, 240, m, resize.Lanczos3)
 					}
 
-					err = ProcessGif(output, pic, tx)
-					if err != nil {
-						log.Println("error processing gif: ", err)
+					err1 := ProcessGif(output, pic, tx)
+					pic.Seek(0, 0)
+					err2 := Thumbnail(thumboutput, pic)
+
+					fi, fierr := os.Stat(otpth + "_resized.gif")
+					if fierr != nil {
+						log.Println("error processing gif: ", err1)
+					}
+					// get the size
+					size := fi.Size()
+					if size > 500000 {
+						log.Println("Image File size:", size)
+					}
+					if err1 != nil {
+						log.Println("error processing gif: ", err1)
+					}
+					if err2 != nil {
+						log.Println("error thumb gif: ", err2)
 					}
 				} else {
 					log.Println("image name:", format)
@@ -93,6 +116,19 @@ func imageToPaletted(img image.Image, p color.Palette) *image.Paletted {
 // TransformFunc is a function that transforms an image.
 type TransformFunc func(image.Image) image.Image
 
+func Thumbnail(w io.Writer, r io.Reader) error {
+	im, err := gif.DecodeAll(r)
+	if err != nil {
+		return err
+	}
+	firstFrame := im.Image[0]
+	b := image.Rect(0, 0, 120, 120)
+	dst := image.NewRGBA(b)
+	draw.CatmullRom.Scale(dst, dst.Rect, firstFrame, firstFrame.Bounds(), draw.Over, nil)
+	log.Println("Gif thumbing:", w)
+	return png.Encode(w, dst)
+}
+
 // ProcessGif the GIF read from r, applying transform to each frame, and writing
 // the result to w.
 func ProcessGif(w io.Writer, r io.Reader, transform TransformFunc) error {
@@ -109,6 +145,9 @@ func ProcessGif(w io.Writer, r io.Reader, transform TransformFunc) error {
 
 	// Create a new RGBA image to hold the incremental frames.
 	firstFrame := im.Image[0].Bounds()
+	// if firstFrame.Dx() != firstFrame.Dy() {
+	// 	return errors.New("first frame must be square")
+	// }
 	b := image.Rect(0, 0, firstFrame.Dx(), firstFrame.Dy())
 	img := image.NewRGBA(b)
 
@@ -134,8 +173,8 @@ func ProcessGif(w io.Writer, r io.Reader, transform TransformFunc) error {
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Process Sticker")
-	content := widget.NewButton("Click to Select Folder", func() {
+	myWindow := myApp.NewWindow("动图处理")
+	content := widget.NewButton("Select folder", func() {
 		dialog.ShowFolderOpen(func(list fyne.ListableURI, err error) {
 			if err != nil {
 				dialog.ShowError(err, myWindow)
@@ -147,11 +186,11 @@ func main() {
 				return
 			}
 
-			children, err := list.List()
-			if err != nil {
-				dialog.ShowError(err, myWindow)
-				return
-			}
+			// children, err := list.List()
+			// if err != nil {
+			// 	dialog.ShowError(err, myWindow)
+			// 	return
+			// }
 			folderPath := list.Path()
 			log.Println(folderPath)
 			_, filesErr := os.Stat(folderPath)
@@ -159,8 +198,22 @@ func main() {
 				dialog.ShowError(filesErr, myWindow)
 				return
 			}
-			processImage(folderPath)
-			out := fmt.Sprintf("Folder %s (%d children):\n%s", list.Name(), len(children), list.String())
+			fileInfo, errfilePath := os.Stat(folderPath)
+			if errfilePath != nil {
+				log.Println(errfilePath, "filePath")
+			}
+			if fileInfo.IsDir() {
+				files, filesErr := ioutil.ReadDir(folderPath)
+				if filesErr != nil {
+					log.Println(folderPath)
+				}
+				for _, file := range files {
+					processImage(folderPath + "/" + file.Name())
+				}
+
+			}
+			// processImage(folderPath)
+			out := fmt.Sprintf("Finished folder")
 			dialog.ShowInformation("Folder Open", out, myWindow)
 		}, myWindow)
 	})
